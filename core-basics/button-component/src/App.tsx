b526fc47a2054cf9056dd8077e26177717a6c8a3
mrtewogbade <johndharey@gmail.com>
2023-12-30 10:33:22 +0100

    commit the shittest things

---
+++ core-basics/button-component/src/App.tsx
@@ -0,0 +1,16 @@
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+// (NOT USING EXTERNAL FRAMEWORKS OR PROCESSOR)
+
+
+//1. EXTERNAL
+//2. MODULES
+//3. INLINE
+
+import Button from "./Button";
+
+function App(){
+  return( <Button />)
+}
+
+
+export default App
