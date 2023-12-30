1a4aa0a59ffd339ce98f39002d26bd1bb14d0250
mrtewogbade <johndharey@gmail.com>
2023-12-30 02:00:52 +0100

    playing

---
+++ core-basics/my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food"
+
+function App() {
+  return(
+    <>
+          < Header/>
+           < Food/>
+          < Footer/>
+         
+    </>
+
+  );
+
+
+}
+
+export default App
---
+++ core-basics/my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer (){
+
+    return(
+        <footer>
+            <p>&Copy; {new Date().getFullYear()} tewocodes</p>
+        </footer>
+    )
+
+}
+
+export default Footer
