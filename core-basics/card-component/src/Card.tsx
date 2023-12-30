9e54cf7fcbb45a0d7eaff658503149a37581b9e4
mrtewogbade <johndharey@gmail.com>
2023-12-30 09:40:43 +0100

    making progress

---
+++ core-basics/card-component/src/Card.tsx
@@ -0,0 +1,22 @@
+function Card(){
+
+    return(
+        <div className="card">
+            <img  src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile picture" 
+            
+            style={{
+                width: "10rem",
+                height: "10rem"
+            }}
+            
+            />
+            <h2>Tewogbade</h2>
+            <p>I am a programmer</p>
+
+        </div>
+    )
+
+}
+
+
+export default Card
