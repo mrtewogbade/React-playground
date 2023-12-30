8388b2d39eb5665812b534b6b448c5f3b68e6d55
mrtewogbade <johndharey@gmail.com>
2023-12-30 09:47:58 +0100

    making progress

--- core-basics/card-component/src/Card.tsx
+++ core-basics/card-component/src/Card.tsx
@@ -2,7 +2,7 @@ function Card(){
 
     return(
         <div className="card">
-            <img  src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile picture" 
+            <img className="currentImg" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile picture" 
             
             style={{
                 width: "10rem",
@@ -10,7 +10,7 @@ function Card(){
             }}
             
             />
-            <h2>Tewogbade</h2>
+            <h2 className="cardTitle">Deborah Oladele</h2>
             <p>I am a programmer</p>
 
         </div>
