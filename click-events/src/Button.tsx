903a0fe0bb976169f277d15f776b46ea68a4fb86
mrtewogbade <johndharey@gmail.com>
2024-01-01 03:15:30 +0100

    clean

--- click-events/src/Button.tsx
+++ click-events/src/Button.tsx
@@ -1,26 +1,38 @@
 interface Button{
     count: number,
-    name: string
+    name: string,
+    e: string
 }
 
-function Button(){
-    // const handleClick = () => console.log("Ok I'm here");
-    let count = 0;
+// function Button(){
+//     // const handleClick = () => console.log("Ok I'm here");
+//     let count = 0;
     
-    const handleClick = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} time/s`);
+//     const handleClick = (name) => {
+//         if(count < 3){
+//             count++;
+//             console.log(`${name} you clicked me ${count} time/s`);
             
-        }else{
-            alert('You reached the limit of clicks');
-        }
-    }
+//         }else{
+//             alert('You reached the limit of clicks');
+//         }
+//     }
     
 
-    return(
-        <button onClick={() => handleClick("tewogbade")}>Click Me</button>
+//     return(
+//         <button onClick={() => handleClick("tewogbade")}>Click Me</button>
+//     )
+// }
+
+// export default Button
+
+function Button(){
+    const handleClick = (e) => e.target.textContent = "OUCH";
+
+    return (
+        <button onClick={(e) => handleClick(e)}> Click me</button>
     )
+    
 }
 
 export default Button
