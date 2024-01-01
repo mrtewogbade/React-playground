cc0c9dea8369b359c12e1010ac084c8a632580a9
mrtewogbade <johndharey@gmail.com>
2024-01-01 03:03:26 +0100

    object pushed

--- click-events/src/Button.tsx
+++ click-events/src/Button.tsx
@@ -1,11 +1,25 @@
-
+interface Button{
+    count: number,
+    name: string
+}
 
 function Button(){
-    const handleClick = () => console.log("Ok I'm here");
+    // const handleClick = () => console.log("Ok I'm here");
+    let count = 0;
+    
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+            
+        }else{
+            alert('You reached the limit of clicks');
+        }
+    }
     
 
     return(
-        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => handleClick("tewogbade")}>Click Me</button>
     )
 }
 
