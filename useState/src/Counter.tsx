befcd795c92bf0303b0b88b9b10a8c52a1a75aa8
mrtewogbade <johndharey@gmail.com>
2024-01-03 23:01:33 +0100

    made changes

--- useState/src/Counter.tsx
+++ useState/src/Counter.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 
-function Counter(){
-    const [count, setCount ] = useState(0);
+// function Counter(){
+//     const [count, setCount ] = useState(0);
 
 
-    return (
-        <>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
-            <button>Reset</button>
-            <button>Increment</button>
+//     return (
+//         <>
+//             <button onClick={() => setCount(count - 1)}>Decrement</button>
+//             <button onClick={() => setCount(count)}>Reset</button>
+//             <button onClick={() => setCount(count + 1)}>Increment</button>
+            
         
-        </>
-    )
+//         </>
+//     )
 
-}
+// }
 
 
-export default Counter;
+// export default Counter;
---
+++ useState/src/MyComponent.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function MyComponent(){
+    const [name, setName] = useState;
+
+    return(
+        <div>
+            <p></p>
+        </div>
+    )
+
+}
+
+export default MyComponent;
