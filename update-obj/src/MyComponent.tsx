2d9be205a3923ffd8dd30794a9debc3d2fc9114e
mrtewogbade <johndharey@gmail.com>
2024-01-09 23:35:00 +0100

    handle obj func

---
+++ update-obj/src/MyComponent.tsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+function MyComponent(){
+
+    const [car, setCar] = useState({year: "2024",
+                                    make: "Ford",
+                                    model: "Mustang"});
+
+    function handleYearChange(event){        
+
+    }
+    function handleMakeChange(event){        
+
+    }
+    function handleModelChange(event){        
+
+    }
+                                    
+
+    return(<div>
+        <p></p>
+    </div>)                                
+
+}
+
+
+export default MyComponent      
